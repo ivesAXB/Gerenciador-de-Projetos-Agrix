45a0a0f250444f9d2c66c7aaf1b3b8f80e693abe
ivesAXB <ivesdearruda@hotmail.com>
2026-03-02 08:48:28 -0400

    feat: Modificando path para git pages v3.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // === ADICIONE ESTA LINHA AQUI ===
-  // Atenção: As barras no começo e no final são obrigatórias!
-  base: "/Gerenciador-de-Projetos-Agrix/", 
+export default defineConfig(({ command, mode }) => ({
+  // A MÁGICA: Se for build (GitHub Pages), usa a pasta. Se for dev (Local), usa a raiz.
+  base: command === 'build' ? '/Gerenciador-de-Projetos-Agrix/' : '/',
   
   server: {
     host: "::",
