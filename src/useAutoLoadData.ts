1261a34df2dd6b97c010d2472581117925beb7bb
ivesAXB <ivesdearruda@hotmail.com>
2026-03-02 14:37:53 -0400

    feat: Atualização para dados estáticos v2.0

--- src/useAutoLoadData.ts
+++ src/useAutoLoadData.ts
@@ -6,7 +6,7 @@ export const useAutoLoadData = (onLoad: (data: ClientData) => void) => {
   useEffect(() => {
     const carregarArquivos = async () => {
       // Nomes exatos que estão na sua pasta public/data/
-      const listaArquivos = ['12026-02-24T19_31_04.117Z LBRIT - Implantacao Agri X - Implantacao Ativa Projetos Em Andamento - Pomar Agricola.xlsx', '22026-02-24T19_29_48.528Z LBRIT - Implantacao Agri X - Implantacao Ativa Projetos Em Andamento - Fazenda Bodoquena.xlsx', '32026-02-24T19_29_14.758Z LBRIT - Implantacao Agri X - Implantacao Ativa Projetos Em Andamento - Grupo Monte Cristo (1).xlsx'];
+      const listaArquivos = ['Pomar Agricola.xlsx', 'Fazenda Bodoquena.xlsx', 'Grupo Monte Cristo.xlsx'];
 
       for (const nome of listaArquivos) {
         try {
